c82e09a8465add82284e731eff7099630b198ce3
Ekolorde <Echolord@protonmail.com>
2024-03-22 15:52:53 -0700

    2015 Day 2 but it works now yay
    Turns out the issue was that the buffer for fgets was too small. That and I was doing the math entirely the wrong way.
    
    Also changed a few other things, like the Readme file, changed the name of the output folder, and removed unnecessary ;s from Day 1.

--- 2015/aoc-2015-d2.c
+++ 2015/aoc-2015-d2.c
@@ -1,25 +1,17 @@
 /* Advent of Code 2015 Day 2: I Was Told There Would Be No Math.
+Link: https://adventofcode.com/2015/day/2
 1: Determine how much wrapping paper to order via math:
-    - Read a very long file consisting of strings like 10x3x28 and 8x4x11, each on it's own line.
-    - Split each line into three integers, somehow.
-    - Determine which of those integers is the smallest, add it to a result variable.
-    - Add each integer together, then multiply that by 2, add to the result variable.
+    - Read a 1000 line file consisting of strings like 10x3x28 and 8x4x11, each on its own line.
+    - Split each line into three integers.
+    - Determine which of those integers is the smallest, add it to a result variable called paper.
+    - Add the results of (l*w + w*h + h*l)*2 to paper.
 2: Determine how much ribbon to order via even more math:
     - Do the first two steps of the first part.
-    - Add the first two integers to a second result variable twice.
-    - multiply all three integers, then add the result of that to the second result variable.
+    - Add the smallest two integers to a result variable called ribbon twice.
+    - multiply all three integers, then add the result of that to ribbon.
 This program requires a file named aoc-2015-d2.txt to function properly, text file must contain 1000 line puzzle input.
-If successful, the output should be 1598415 for 1, and 3812909 for 2.
-*/
-/* Things I need to learn in order to do this:
-
-[x] How to read through a file, line by line.
-[x] How to split a line into three parts, while tossing the x's.
-[x] How to convert a string into an integer
-[ ] why the hell doesn't this work god damn it
-
-Other than that I have a pretty good idea of what I need to do.
-I'm just going to write around the stuff I don't know how to do and then come back to that later.
+If successful, using included input file, the output should be 1598415 for 1, and 3812909 for 2.
+Functional when compiled with tdm-gcc using the C/C++ Compile Run extension for Visual Studio Code as of March 22, 2024.
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,67 +21,113 @@ int main() {
     FILE* fptr;
     int paper = 0;
     int ribbon = 0;
-    int first, second, third, i, i2;
-    int lines = 1000;
-    const int buflen = 9;
-    char buf[buflen];
+    int l, w, h, i, i2;
+    int lines = 1000; // Set this number to the number of lines in your input file if it's different for some reason.
+    const int buflen = 15;
+    char buf[15];
 
     fptr = fopen("aoc-2015-d2.txt", "r");
 
-    for (i = 0; i < lines; i++) {
+    for (i = 0; i < lines; i += 1) {
         fgets(buf, buflen, fptr);
-        printf("%s", buf);
+        //printf("%d %s", i, buf); // This is just here for debugging, everything works now but I'm keeping it because it looks neat when it's uncommented.
         char* token = strtok(buf, "x");
         i2 = 0;
         while (token != NULL) {
             if (i2 == 0) {
-                first = atoi(token);
-                printf("%d ", first);
+                l = atoi(token);
             }
             else if (i2 == 1) {
-                second = atoi(token);
-                printf("%d ", second);
+                w = atoi(token);
             }
             else if (i2 == 2) {
-                third = atoi(token);
-                printf("%d\n", third);
+                h = atoi(token);
             }
             token = strtok(NULL, "x");
             i2 += 1;
         }
 
-        if (first <= second && first <= third) {
-            paper += first;
+        //  Add smallest number to paper. Multiply each number by 2, add to paper.
+        if ((l * w) <= (w * h) && (l * w) <= (h * l)) {
+            paper += l * w;
         }
-        else if (second <= first && second <= third) {
-            paper += second;
+        else if ((w * h) <= (l * w) && (w * h) <= (h * l)) {
+            paper += w * h;
         }
-        else if (third <= first && third <= second) {
-            paper += third;
+        else if ((h * l) <= (l * w) && (h * l) <= (w * h)) {
+            paper += h * l;
         }
-        paper += first * 2;
-        paper += second * 2;
-        paper += third * 2;
-        printf("%d ", paper);
+        paper += 2 * l * w;
+        paper += 2 * w * h;
+        paper += 2 * h * l;
 
-        ribbon += first * 2;
-        ribbon += second * 2;
-        ribbon += first * second * third;
-        printf("%d\n", ribbon);
+        // To ribbon: Add two smallest numbers twice, add the result of multiplying all three numbers.
+        // This is a whole lot of repetitive code for something like this but I don't know how to do it differently yet.
+        if (l <= w && l <= h) {
+            ribbon += l * 2;
+            if (w <= h) {
+                ribbon += w * 2;
+            }
+            else {
+                ribbon += h * 2;
+            }
+        }
+        else if (w <= l && w <= h) {
+            ribbon += w * 2;
+            if (l <= h) {
+                ribbon += l * 2;
+            }
+            else {
+                ribbon += h * 2;
+            }
+        }
+        else {
+            ribbon += h * 2;
+            if (w <= l) {
+                ribbon += w * 2;
+            }
+            else {
+                ribbon += l * 2;
+            }
+        }
+        ribbon += l * w * h;
     }
     printf("\nPaper: %d\n", paper);
     printf("Ribbon: %d", ribbon);
 
+    fclose(fptr);
     return 0;
 }
-/*
-Okay I've actually made an attempt at debugging now and I've found a few issues:
-
-1: My compiler outputs .exe files to a subfolder of where the .c file is stored, while the program assumes the needed txt file will be in the exact same folder as it. That was fixed by temporarily moving the text file.
-2: My fgets stops fgetting at line 750 for some reason.
-3: (now that the line splitter should be working properly) paper is way too low and ribbon is way too high. Like if it were just that they were both a little too low I'd pin it on the thing stopping at 750, but ribbon is *millions* above the correct answer, and paper is at a hundred thousand when it should be 1.5 million.
-
-I really don't know where the issue is with this thing, but I think it can be blamed on the fact that I have no idea what I'm doing or how half the code in this actually works.
-
-The above no longer applies, I've made a few changes and this seems fully functional outside of the part where it always stops at line 750, which I still have no explanation for. Gonna come back to this thing tomorrow.
+/* How this works and what I learned:
+1: Include stdio.h, stdlib.h, and string.h. stdio for printf and file reading, stdlib for atoi, and string.h for strtok.
+2: Declare int main. That's gonna happen every time so I'm gonna stop mentioning it after this.
+3: Set a whole ton of variables:
+    - FILE* fptr is required to do anything with files it seems.
+    - int paper and int ribbon to keep track of final puzzle answers.
+    - ints l, w, and h to keep track of the three numbers on each line of the input file.
+    - ints i and i2 for looping purposes.
+    - int lines to store the length of the input in lines and stop the main loop once it reaches that point.
+    - const int buflen because it's required for fgets, to set a limit to the next thing.
+    - char buf[15] to store the current line of the input file.
+        - I spent ~2 days trying and failing to get this thing to work, the entire time it seems the problem was that the buffer was too small. (It was at 9, which I thought was the longest one of the input lines could be) So apparently giving your buffer some extra room is necessary.
+4: fopen the input file in "r" mode. "rb" apparently also works fine, even though the input is a .txt file.
+    - As I was writing this I started freaking out a bit because despite having not changed anything the whole program stopped working right. The issue was that the input file was in the wrong folder. ALWAYS put the input file in the same folder or it doesn't work at all. I keep forgetting that.
+5: Run a for loop that runs once for every line of the input file.
+    5.1: Run fgets(buf, buflen, fptr) to get the next input line.
+    Bonus thing: Uncomment that printf line and it'll print the contents of each line alongside the appropriate line number (minus one)
+    5.2: I admit I don't entirely understand how this bit works, but this uses strtok to split the input line into three pieces, discarding the xs.
+    5.3: Set i2 to 0, resetting it every time the for loop runs.
+    5.4: Run a while loop that runs through the output of strtok until it hits NULL.
+        5.4.1: Convert the three strings strtok outputs into integers, assigning them to the appropriate variables, through a lot of ifs and atoi.
+            - So i2 is always going to be either 0, 1 or 2, corresponding to the 3 output strings of strtok.
+            - Depending on which of those three numbers i2 is at the moment, this sets the corresponding variable to the corresponding string.
+        5.4.2: Move to the next output string of strtok.
+        5.4.3: Increase i2 by 1.
+    5.5: Do a whole lot of math (already explained at the top of file and the top of the relevant section of code) using a whole bunch of repetitive if statements and math operators and such.
+        - Reminder to always make sure you *know* what the puzzle is asking you to do. When I first wrote all the math stuff I had entirely the wrong idea in that regard, and ended up needing to rewrite the thing entirely once I finally got the file reading part to work.
+        - This also got me to learn how to search + replace stuff in VSCode, because originally I had been calling l w and h first second and third respectively, which took up a lot more space.
+6: Print the result variables paper and ribbon.
+    - The printf for paper starts with a newline character. This is only really needed if you have the printf I mentioned earlier that prints the contents of each line uncommented, otherwise it's not needed.
+7: Close the input file.
+8: Return 0 to tell anything using this that it was successful.
 */
--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Advent-of-C
 My solutions to Advent of Code puzzles, in C specifically.
 
+All of the programs that require an input file have one included in the "Compiled programs" folder, but should work with any other official puzzle input. (Official in this case meaning obtained from the AoC website)
+
 Credit to [Advent of Code](https://adventofcode.com/) for the puzzles. And credit to the kind folks of [Stack Overflow,](https://stackoverflow.com/) [Geeks for Geeks](https://www.geeksforgeeks.org) and the internet in general for teaching me the stuff I needed to know to solve them
 
-All code in this repo is subject to the Unlicense.
+All code in this repo is subject to the Unlicense, except for included C libraries I didn't make, (stdio.h, string.h, stdlib.h, that sort of thing) I don't actually know how those are licensed.
 
 Everything past this point is just quick descriptions of what each file does. More information can be found in the files themselves.
 
@@ -11,4 +13,4 @@ Everything past this point is just quick descriptions of what each file does. Mo
 
 [**Day 1: Not Quite Lisp.**](https://adventofcode.com/2015/day/1) Finds the solution to its respective puzzle using a built in puzzle input, making it pretty useless unless you go in and edit the input variable yourself before compiling.
 
-[**Day 2: I Was Told There Would Be No Math.**](https://adventofcode.com/2015/day/2) Does a bunch of fancy math using a separate input file located in the same folder with the same filename but a .txt file extension. Or it would if I could get the damn thing to work.
+[**Day 2: I Was Told There Would Be No Math.**](https://adventofcode.com/2015/day/2) Does a bunch of fancy math using a separate input file located in the same folder with the same filename but a .txt file extension.
